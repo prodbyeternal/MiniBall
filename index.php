252fa02413dee0319aa17f8502eb64bb7a1ea57c
eternal <eternal@eternals-MacBook-Pro.local>
2026-09-24 20:17:56 +0200

    fix3

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <title>MiniBall</title>
 <link rel="stylesheet" href="assets/style.css">
+<link rel="icon" type="image/x-icon" href="assets/1099672.png">
 </head>
 <body>
 
